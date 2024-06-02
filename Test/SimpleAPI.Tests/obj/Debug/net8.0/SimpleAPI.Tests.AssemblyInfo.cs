7243a687e92121af4f7583cd610ecf10278c14d0
aaasji <aashisharora2017@gmail.com>
2024-06-02 20:33:31 +0530

    Updated azure-pipelines.yml to publish build

--- Test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
+++ Test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15599f31f44e98125ce49aa4212a23231e9ee0c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e6e41c2dee51dcc84606a8fbc13ee4b39aeadc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
