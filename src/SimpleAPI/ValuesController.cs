adb950e450bceb45588dc9bb41dae83c3ea023ea
aaasji <aashisharora2017@gmail.com>
2024-06-09 14:18:08 +0530

    Initial Commit

--- src/SimpleAPI/ValuesController.cs
+++ src/SimpleAPI/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI
         // GET: api/values/5
         public string Get(int id)
         {
-            return "SanjayArora";
+            return "AshishArora";
         }
 
         // POST: api/values
